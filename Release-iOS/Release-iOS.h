af6c5e1c1b324fccf30b8590b5adf689462bfbd1
Luke Durrant <contact@lukedurrant.com>
2016-09-27 15:37:19 +1000

    Release-iOS

---
+++ Release-iOS/Release-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Release-iOS.h
+//  Release-iOS
+//
+//  Created by Luke Durrant on 27/09/2016.
+//  Copyright © 2016 layerhq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Release-iOS.
+FOUNDATION_EXPORT double Release_iOSVersionNumber;
+
+//! Project version string for Release-iOS.
+FOUNDATION_EXPORT const unsigned char Release_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Release_iOS/PublicHeader.h>
+
+
